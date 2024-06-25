6dd9f8f3adc991ecd827b0b910fadd653e7888d7
mpochard <88907375+m3L4n@users.noreply.github.com>
2024-06-25 16:49:11 +0200

    feat : print like method  pandas describe

--- describe.py
+++ describe.py
@@ -14,7 +14,6 @@ from utils.max import max
 from utils.min import min
 
 # TO DO DELETE NULL SEGMENT
-# TO DO IMPLEMENT THE correct print
 
 def load(path: str) -> pd.DataFrame | None:
     """Take in parameter a csv and return its dataframe."""
@@ -36,6 +35,40 @@ def define_numbers_features(data_csv):
     row_numeric = dict([(x, list(data_csv[x])) for x in column if is_numeric_dtype(data_csv[x])])
     return row_numeric
 
+def print_pandas_describe(numeric_features : dict):
+    """Print like the method describe of Pandas."""
+    key = numeric_features.keys()
+    key_padded = [x[0:15].ljust(15, ' ')  for x in numeric_features.keys()]
+    count_list = [str(count(numeric_features[x])).ljust(15, ' ') for x in key]
+    mean_list = [str(mean(numeric_features[x])).ljust(15, ' ') for x in key]
+    std_list = [str(std(numeric_features[x])).ljust(15, ' ') for x in key]
+    min_list = [str(min(numeric_features[x])).ljust(15, ' ') for x in key]
+    fp_list = [str(percentile(numeric_features[x], 0.25)).ljust(15, ' ') for x in key]
+    sp_list = [str(percentile(numeric_features[x], 0.50)).ljust(15, ' ') for x in key]
+    tp_list = [str(percentile(numeric_features[x], 0.75)).ljust(15, ' ') for x in key]
+    max_list = [str(max(numeric_features[x])).ljust(15, ' ') for x in key]
+    sum_list = [str(sum(numeric_features[x])).ljust(15, ' ') for x in key]
+    fq_list = [str(f_quartile(numeric_features[x])).ljust(15, ' ') for x in key]
+    sq_list = [str(s_quartile(numeric_features[x])).ljust(15, ' ') for x in key]
+    tq_list = [str(t_quartile(numeric_features[x])).ljust(15, ' ') for x in key]
+    print(" .    " ,  '\t'.join(key_padded) )
+    print("Count ",'\t'.join(count_list))
+    print("Mean  ",'\t'.join(mean_list))
+    print("Std   ",'\t'.join(std_list))
+    print("Min   ",'\t'.join(min_list))
+    print("25%   ",'\t'.join(fp_list))
+    print("50%   ",'\t'.join(sp_list))
+    print("75%   ",'\t'.join(tp_list))
+    print("Max%  ",'\t'.join(max_list))
+    print("Sum   ",'\t'.join(sum_list))
+    print("0.25 Q",'\t'.join(fq_list))
+    print("0.50 Q",'\t'.join(sq_list))
+    print("0.75 Q",'\t'.join(tq_list))
+    
+    
+    
+    
+    
 
 def main(argv):
     """Main function."""
@@ -44,21 +77,7 @@ def main(argv):
         path = argv[1]
         data_csv = load(path)
         numeric_features = define_numbers_features(data_csv)
-        for key,value in numeric_features.items():
-          print("KEY :", key)
-          print (f"count {count(value)}")
-          print (f"Mean {mean(value)}")
-          print (f"Std {std(value)}")
-          print (f"25Q {f_quartile(value)}")
-          print (f"50Q {s_quartile(value)}")
-          print (f"75Q{t_quartile(value)}")
-          print (f"25% {percentile(value, 0.25)}")
-          print (f"50% {percentile(value, 0.5)}")
-          print (f"75% {percentile(value, 0.75)}")
-          print (f"max {max(value)}")
-          print (f"min {min(value)}")
-          print (f"Sum {sum(value)}\n")
-
+        print_pandas_describe(numeric_features)
     except Exception as e:
         print(type(e).__name__, ":", str(e))
 
