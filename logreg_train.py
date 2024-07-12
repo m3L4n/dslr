2613843c9356e06a5147daea7d65cf00b4c49992
macrespo42 <macrespo@student.42.fr>
2024-07-12 12:07:00 +0200

    feat: normalize X

--- logreg_train.py
+++ logreg_train.py
@@ -3,8 +3,10 @@
 import math
 
 import pandas as pd
+import numpy as np
 
 from model.LogisticRegression import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 
 def mean(data) -> float:
@@ -32,6 +34,19 @@ def transform_nan_to_mean(data_csv):
     return data_cpy
 
 
+def standardization(x):
+    """Standardize x based on z-score algorithm.
+
+    standardization(x)
+    """
+    mean = np.mean(x, axis=0)
+    std = np.std(x, axis=0)
+    x_norm = x.copy()
+    for i in range(x.shape[1]):
+        x_norm.iloc[:i] = (x.iloc[:i] - mean.iloc[i]) / std.iloc[i]
+    return x_norm
+
+
 def train() -> None:
     """Create a LogisticRegression instance and use it to train our model.
 
@@ -55,14 +70,18 @@ def train() -> None:
     )
 
     x_train = transform_nan_to_mean(x_train)
+    x_norm = standardization(x_train)
     logreg = LogisticRegression()
 
-    logreg.fit(x_train, y_train)
+    logreg.fit(x_norm, y_train)
 
     logreg.save(x_train.columns.values)
-    print(logreg.predict(x_train))
+    pred_y = logreg.predict(x_train)
+
+    true_pred = np.count_nonzero(pred_y == y_train)
+    print((true_pred / 1600) * 100)
+    print(accuracy_score(pred_y, y_train) * 100)
 
 
 if __name__ == "__main__":
     train()
-    griffyndor = pd.read_csv("datasets/Gryffindor_weights.csv")
